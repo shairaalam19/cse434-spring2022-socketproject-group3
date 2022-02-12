532c4420472a04b87076160bc99e9e0f266d38a4
Shaira Alam <shaira.alam19@gmail.com>
2022-02-11 19:34:18 -0700

    basic UDPServer.py algo

--- UDPClient.py
+++ UDPClient.py
@@ -1,13 +1,17 @@
-# be able to create a socket 
-from socket import *
-# Pass either the IP address or the hostname of the server (like general.asu.edu)
-serverName = 'hostname' 
-serverPort = 12000
-# AF_INET means IPv4 
-# SOCK_DGRAM means UDP
-clientSocket = socket(AF_INET, SOCK_DGRAM)
-message = raw_input('Input lowercase sentence:')
-clientSocket.sendto(message.encode(),(serverName, serverPort))
-modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
-print(modifiedMessage.decode())
-clientSocket.close()
+# import socket 
+# # be able to create a socket 
+# from socket import *
+# # Pass either the IP address or the hostname of the server ((do ifconfig to get the IP Address)
+# serverName = '10.120.70.106' 
+# serverPort = 2600
+# # AF_INET means IPv4 
+# # SOCK_DGRAM means UDP
+# clientSocket = socket(AF_INET, SOCK_DGRAM)
+# # raw_input for python 2.x
+# message = input('Input lowercase sentence:') 
+# # converts to byte type 
+# clientSocket.sendto(message.encode(),(serverName, serverPort))
+# modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
+# print(modifiedMessage.decode())
+# clientSocket.close()
+
--- UDPServer.py
+++ UDPServer.py
@@ -1,9 +1,75 @@
-from socket import *
-serverPort = 12000
-serverSocket = socket(AF_INET, SOCK_DGRAM)
-serverSocket.bind(('', serverPort))
-print("The server is ready to receive")
-while True:
-    message, clientAddress = serverSocket.recvfrom(2048)
-    modifiedMessage = message.decode().upper()
-    serverSocket.sendto(modifiedMessage.encode(), clientAddress)
+# from socket import *
+# serverPort = 2600
+# serverSocket = socket(AF_INET, SOCK_DGRAM)
+# serverSocket.bind(('', serverPort))
+# print("The server is ready to receive")
+# while True:
+#     message, clientAddress = serverSocket.recvfrom(2048)
+#     modifiedMessage = message.decode().upper()
+#     serverSocket.sendto(modifiedMessage.encode(), clientAddress)
+
+# Players
+# port = 2600
+# IP = '10.120.70.106'
+# location = [IP, port]
+# name = 'Tao'
+# player1 = dict({name: location})
+# print(player1)
+
+# dictionary of dictionaries (players is a dictionary containing a dictionary for every player)
+players = {} 
+
+# games is a dictionary with the following
+# key: game counter 
+# value: dictionary for every single game 
+gameCounter = 1;
+games = {}
+
+# game dictionary
+# key = gameCount (and then increment so next game can have that identifier)
+# value: list of players with players[0] = dealer 
+
+def register(name, IP, port ):
+    print("Inside register()")
+    location = [IP, port]
+    player = dict({name: location})
+    players.update(player) 
+    print(player)
+    print("\n")
+
+def queryPlayers():
+    print("Inside queryPlayers()")
+    print(players)
+    print("\n")
+
+def startGame(dealer, user):
+    print("Inside startGame()")
+    print("\n")
+
+def queryGames():
+    print("Inside queryGames()")
+    print(games)
+    print("\n")
+
+def end(id, user):
+    print("Inside startGame()")
+    print("\n")
+ 
+def deregister(name):
+    print("Inside deregister()")
+    players.pop(name)
+    print(players)
+    print("\n")
+
+def main():
+    register("Tao", "10.120.70.106", 2600)
+    register("Brenda", "10.120.70.106", 2600)
+    queryPlayers()
+    # queryGames()
+    deregister("Tao")
+    deregister("Brenda")
+
+if __name__ == "__main__":
+    main()
+
+    
